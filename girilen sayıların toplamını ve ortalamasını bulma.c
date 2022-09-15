c0be06c461a3658aa44e83138a03947720620dab
kavrayantunay <70447638+kavrayantunay@users.noreply.github.com>
2022-09-15 21:37:44 +0300

    Add files via upload

---
+++ girilen sayıların toplamını ve ortalamasını bulma.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(){
+    int top=0;
+    int say=0;
+    int sayi=0;
+    while(sayi!=-1){
+        printf("bir sayi giriniz");
+        scanf("%d",&sayi);
+        if(sayi==-1)
+        break;
+        top=top+sayi;
+        say++
+        printf("top:%d ")
+    }
+}
